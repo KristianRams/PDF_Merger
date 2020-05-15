883c738712d57b7488166ea079b3693e95545a73
KristianRams <44986515+KristianRams@users.noreply.github.com>
2020-05-15 01:21:51 -0400

    Better error gui

--- pdf_combine.py
+++ pdf_combine.py
@@ -1,46 +1,48 @@
 # System imports 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import sys
-import easygui
-from easygui import*
+from easygui import *
 
 # File imports
 def combine_pdf() -> bool: 
     arr_of_input_pdfs: List[PdfFileReader] = []
     state: bool = False
-    default_path: str = 'C:\\' 
+    default_path: str = 'C:\\'
     while 1:
-        if state == False: 
-            user_response: str = easygui.buttonbox(msg='Select a PDF to merge', title='PDF Merger', choices=('Select PDF', 'Cancel'))
+        if state == False:
+            user_response: str = buttonbox(msg='Select a PDF to merge', title='PDF Merger', choices=('Select PDF', 'Cancel'))
             if user_response == 'Select PDF':
-                f: str = easygui.fileopenbox(default=default_path)
+                f: str = fileopenbox(default=default_path)
                 if (f is None):
-                    continue 
-                    # sys.exit(1) 
-                ff: str = f.split('\\')
-                ff = ff[:-1] 
-                ff = "\\".join(ff) + "\\"
-                default_path = ff
-                if (f[-4:] != ".pdf"): 
-                    print("Error: File selected is not a PDF. Please select a PDF.")
+                    # @Note:
+                    # Depending on your defintion of what
+                    # should happen when you exit file explorer
+                    # without selecting a file, either cancel the entire
+                    # process or let the user have another opportunity to
+                    # select another PDF or Finish, currently I do the latter
+                    # sys.exit(1)
                     continue
-                arr_of_input_pdfs.append(PdfFileReader(open(f, 'rb'))) 
+                default_path =  "\\".join(f.split('\\')[:-1]) + "\\"
+                if (f[-4:] != ".pdf"):
+                    ccbox(msg="Error: File selected is not a PDF. Please select a PDF.", title="Error")
+                    # print("Error: File selected is not a PDF. Please select a PDF.")
+                    continue
+                arr_of_input_pdfs.append(PdfFileReader(open(f, 'rb')))
                 state = True
             else:
                 sys.exit(1)
+        # @Copy n' Paste from above
         elif state:
-            user_response: str = easygui.buttonbox(msg='Select another PDF to merge', title='PDF Merger', choices=('Select another PDF', 'Finish'))
+            user_response: str = buttonbox(msg='Select another PDF to merge', title='PDF Merger', choices=('Select another PDF', 'Finish'))
             if user_response == 'Select another PDF':
-                f: str = easygui.fileopenbox(default=default_path)
+                f: str = fileopenbox(default=default_path)
                 if (f is None): 
                     continue
+                    # See @Note above
                     # sys.exit(1) 
-                ff: str = f.split('\\')
-                ff = ff[:-1] 
-                ff = "\\".join(ff) + "\\"
-                default_path = ff
+                default_path =  "\\".join(f.split('\\')[:-1]) + "\\"
                 if (f[-4:] != ".pdf"): 
-                    print("Error: File selected is not a PDF. Please select a PDF.")
+                    ccbox(msg="Error: File selected is not a PDF. Please select a PDF.", title="Error")
                     continue
                 arr_of_input_pdfs.append(PdfFileReader(open(f, 'rb'))) 
             elif user_response == 'Finish': 
@@ -62,7 +64,7 @@ def combine_pdf() -> bool:
 
 def main() -> None:
     if(combine_pdf()): 
-        print("Success")
+        print("Success, Goodbye!")
 
 if __name__ == "__main__":
     main()
